d135f2dc6a5cb2d745bab46347881f2a6082437e
Kuntal <kkargupta007@gmail.com>
2024-12-14 02:05:28 +0530

    Update

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://socialtunes-tawny.vercel.app",
+    origin: "https://social-tunes.vercel.app/",
   },
 });
 
