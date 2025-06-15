00aa8448d8fed7a60c165a303212e40aad21afce
Kuntal Kargupta <167575898+thekuntal49@users.noreply.github.com>
2025-06-15 10:44:01 +0530

    Changes

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://socialtunes.onrender.com/api",
+  baseURL: "https://social-tunes.onrender.com/api",
   // baseURL: "http://192.168.1.2:5000/api",
   headers: {
     "Content-Type": "application/json",
